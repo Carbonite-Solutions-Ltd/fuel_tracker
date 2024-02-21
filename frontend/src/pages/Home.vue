2e12c17c701c1cfd16629008b55a2333082c0250
carbonite solutions <admin@carbonitesolutions.com>
2024-02-21 15:16:00 -0500

    feat: added cards and fetching list from doctype used fuel

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,9 +1,42 @@
 <template>
+<!-- <div class="max-w-3xl py-12 mx-auto">
+  <ul>
+    <li v-for="action in actions.data" :key="action.name"></li>
+    {{ action.name }}
+  </ul>
+</div> -->
 
-<h1 class="app-h1" >Fuel Tracker</h1>
-
+<Button @click="dialog1 = true">
+  Show Dialog
+</Button>
+<Dialog
+  :options="{
+    title: 'Confirm',
+    message: 'Are you sure you want to confirm this action?',
+    size: 'xl',
+    actions: [
+      {
+        label: 'Confirm',
+        variant: 'solid',
+        onClick: () => {
+                },
+      },
+    ],
+  }"
+  v-model="dialog1"
+/>
 </template>
 
-<script >
+<script>
+import { Dialog, createListResource } from 'frappe-ui'
+
+// const actions = createListResource({
+//   doctype: 'Make',
+//   fields: ["name"],
+//   limit: 100
+// })
+
+// actions.reload()
 </script>
+
 
