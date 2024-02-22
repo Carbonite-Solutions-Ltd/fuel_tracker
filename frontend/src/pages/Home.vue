cb1006be174e0dd618ab04403b7887b63759b720
carbonite solutions <admin@carbonitesolutions.com>
2024-02-22 05:52:31 -0500

    feat: added icon and upload image

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -2,13 +2,18 @@
     <ion-page>
     <ion-header>
       <ion-toolbar>
+        <div class="flex flex-row items-center justify-between mt-2 mr-2 ml-0">
         <ion-title>Fuel Tracker</ion-title>
+        <div>
+        <FeatherIcon @click="issueFuelDialogShown = true" src="././public/favicon.png" style="width: 24px; height: 24px;" />
+        </div>   
+        </div>
       </ion-toolbar>
     </ion-header>
   </ion-page>
 
   <div class="flex flex-row items-center justify-between mt-2 mr-2 ml-2">
-    <h1 class="text-5xl font-black text-gray-900">Fuel Tracker</h1>
+    <h1 class="text-5xl font-black text-gray-900"></h1>
     <Button @click="issueFuelDialogShown = true" icon-left="plus">Issue Fuel</Button>
 </div>
 
@@ -37,12 +42,77 @@
   ],              
 }" v-model="issueFuelDialogShown">
 <template #body-content>
-  <div>
-    <Autocomplete :options="ListOfSite" Variant title="Single option" placeholder= "Select Site" />
-    <Autocomplete Variant title="Single option" placeholder= "Select Fuel Tanker"/>
-    <Autocomplete Variant title="Single option" placeholder= "Select Resource" />
-    <Input type="text" placeholder= "Enter Odometer" required/>
-    <Input type="text" placeholder= "Enter Fuel Issued(LTS)" required/>
+  <div class="space-y-4">
+    <Variant title="Single option">
+      <div class="p-2">
+        <Autocomplete
+          :options="ListOfSite"
+          v-model="sites.name"
+          placeholder="Select Site"
+        />
+      </div>
+    </Variant>
+    <Variant title="Single option">
+      <div class="p-2">
+        <Autocomplete
+          :options="ListOfFuelTanker"
+          v-model="fuelTankers.tanker"
+          placeholder="Select Fuel Tanker"
+        />
+      </div>
+    </Variant>
+    <Variant title="Single option">
+      <div class="p-2">
+        <Autocomplete
+          :options="ListOfResource"
+          v-model="Resources.name"
+          placeholder="Select Resource"
+        />
+      </div>
+    </Variant>
+    <Variant>
+      <div class="p-2">
+        <Input
+        type="number"
+          placeholder="Type In Odometer Reading"
+        />
+      </div>
+    </Variant>
+    <Variant>
+      <div class="p-2">
+        <Input
+        type="number"
+          placeholder="Type In Fuel Issued (LTS)"
+        />
+      </div>
+    </Variant>
+    <Variant>
+    <div class="p-2">
+    <FileUploader
+      :fileTypes="['image/*']"
+      :validateFile="validateFileFunction"
+      @success="onSuccess"
+    >
+      <template
+        v-slot="{
+          file,
+          uploading,
+          progress,
+          uploaded,
+          message,
+          error,
+          total,
+          success,
+          openFileSelector,
+        }"
+      >
+        <Button @click="openFileSelector" :loading="uploading">
+          {{ uploading ? `Uploading ${progress}%` : 'Upload Img of Odometer' }}
+        </Button>
+      </template>
+    </FileUploader>
+  </div>
+</Variant>
   </div>
 </template>
 </Dialog>
@@ -54,13 +124,11 @@
 
 import { reactive } from 'vue'
 import { ref, computed } from 'vue'
-import { Button, Dialog, Input, Autocomplete } from 'frappe-ui'
+import { FileUploader, Button, Dialog, Input, Autocomplete, Card, createListResource } from 'frappe-ui'
 import { createResource } from 'frappe-ui'
-import { createListResource } from 'frappe-ui'
 import { session } from '../data/session'
 import { Checkbox } from 'frappe-ui'
 import { Select } from 'frappe-ui'
-import { Card } from 'frappe-ui'
 import { FeatherIcon } from 'frappe-ui'
 
 
@@ -78,9 +146,7 @@ actions.reload()
 const issueFuelDialogShown = ref(false)
 
 const issueFuel = () => {
-  actions.insert.submit({
-
-  })
+  actions.insert.submit(action)
 }
 
 const sites = createListResource({
@@ -96,10 +162,56 @@ const ListOfSite = computed(() => {
   if (sites.list.loading || !sites.data){
     return []
   }
-  
 
   return sites.data
 })
+
+
+
+//List of Fuel Tanker
+const fuelTankers = createListResource({
+  doctype: 'Fuel Tanker',
+  fields: ['tanker', 'tanker'],
+  transform(fuelTankers) {
+    return fuelTankers.map((s) => ({label: s.tanker, value: s.tanker}))
+  }
+})
+fuelTankers.reload()
+
+const ListOfFuelTanker = computed(() => {
+  if (fuelTankers.list.loading || !fuelTankers.data){
+    return []
+  }
+
+  return fuelTankers.data
+})
+
+
+
+
+//List of Resource
+const Resources = createListResource({
+  doctype: 'Resource',
+  fields: ['name', 'name'],
+  transform(fuelTankers) {
+    return fuelTankers.map((s) => ({label: s.name, value: s.name}))
+  }
+})
+Resources.reload()
+
+const ListOfResource = computed(() => {
+  if (Resources.list.loading || !Resources.data){
+    return []
+  }
+
+  return Resources.data
+})
+
+
+
+
+const validateFileFunction = (fileObject) => {}
+const onSuccess = (file) => {}
 </script>
 
 
