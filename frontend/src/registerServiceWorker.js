68e7651d97028aa757add5bfede4bafa63ddec46
Carbonit Solutions <admin@carbonitesolutions.com>
2024-03-01 14:57:53 +0000

    Trying new offline 2

--- frontend/src/registerServiceWorker.js
+++ frontend/src/registerServiceWorker.js
@@ -1,7 +1,7 @@
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('./service-worker.js').then(registration => {
+      navigator.serviceWorker.register('%PUBLIC_URL%/serviceworker.js').then(registration => {
         console.log('SW registered: ', registration);
       }).catch(registrationError => {
         console.log('SW registration failed: ', registrationError);
