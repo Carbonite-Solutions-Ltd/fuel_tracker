63904e4004bded0d65e8d45fd47a2f99cdb17e2b
Carbonit Solutions <admin@carbonitesolutions.com>
2024-04-09 18:05:00 +0000

    Made New Changes

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -30,7 +30,7 @@ app.mount('#app')
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then(registration => {
+    navigator.serviceWorker.register('sw.js').then(registration => {
       console.log('SW registered: ', registration);
     }).catch(registrationError => {
       console.log('SW registration failed: ', registrationError);
