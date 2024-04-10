83587d0af3802079a9b1e01de2296927bd72a591
Carbonit Solutions <admin@carbonitesolutions.com>
2024-04-10 11:35:19 +0000

    Made New Changes 111122

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,11 +1,6 @@
 const CACHE_NAME = 'fuel-tracker-cache-v1';
 const urlsToCache = [
-  "./",
-//   "./src/pages/Home.vue",
-//   "./index.html",
-//   "./src/index.css",
-//   "./src/main.js"
-//   // Add other URLs you want to cache
+  "./logo512.png",
 ];
 
 // Install a service worker
