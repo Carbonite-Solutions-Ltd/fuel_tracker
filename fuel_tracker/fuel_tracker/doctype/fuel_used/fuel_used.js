fb8a4c98bf04c7521de02777f9353dbfd4409c04
Carbonit Solutions <admin@carbonitesolutions.com>
2024-07-25 14:40:55 +0000

    Changes to the fuel supplied

--- fuel_tracker/fuel_tracker/doctype/fuel_used/fuel_used.js
+++ fuel_tracker/fuel_tracker/doctype/fuel_used/fuel_used.js
@@ -1,8 +1,21 @@
 // Copyright (c) 2024, Carbonite Solutions Ltd and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Fuel Used", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Fuel Used", {
+    refresh(frm) {
+        // Custom client-side logic can be added here if needed
+    },
+    validate(frm) {
+        if (frm.doc.resource_type === "Truck") {
+            if (frm.doc.odometer_km < frm.doc.previous_odometer_km) {
+                frappe.msgprint(__("Odometer reading cannot be less than the previous reading: {0} km", [frm.doc.previous_odometer_km]));
+                frappe.validated = false;
+            }
+        } else if (frm.doc.resource_type === "Equipment") {
+            if (frm.doc.hours_copy < frm.doc.previous_hours_copy) {
+                frappe.msgprint(__("hours_copy cannot be less than the previous hours_copy: {0} hours_copy", [frm.doc.previous_hours_copy]));
+                frappe.validated = false;
+            }
+        }
+    }
+});
--- fuel_tracker/fuel_tracker/doctype/fuel_used/fuel_used.py
+++ fuel_tracker/fuel_tracker/doctype/fuel_used/fuel_used.py
@@ -3,10 +3,27 @@ from frappe.model.document import Document
 
 class FuelUsed(Document):
     def on_submit(self):
+        # Perform validation based on resource type
+        self.validate_resource_usage()
+        
         # Get the previous balance or create a new Fuel Balance if it doesn't exist
         previous_balance, balance_doc = self.get_or_create_fuel_balance()
+        
         # Create a Fuel Entry document to record the fuel used
         self.create_fuel_entry(previous_balance)
+        
+        # Update the current odometer or hours in the Resource doctype
+        self.update_resource_usage()
+
+    def validate_resource_usage(self):
+        resource = frappe.get_doc("Resource", self.resource)
+        
+        if self.resource_type == "Truck":
+            if self.odometer_km < self.previous_odometer_km:
+                frappe.throw(f"Odometer reading cannot be less than the previous reading: {self.previous_odometer_km} km")
+        elif self.resource_type == "Equipment":
+            if self.hours_copy < self.previous_hours_copy:
+                frappe.throw(f"Hours cannot be less than the previous hours: {self.previous_hours_copy} hours")
 
     def get_or_create_fuel_balance(self):
         existing_balance = frappe.get_list("Fuel Balance",
@@ -28,8 +45,7 @@ class FuelUsed(Document):
         return balance_doc.balance, balance_doc
 
     def create_fuel_entry(self, previous_balance):
-        # Assume fuel_issued_lts and other relevant fields are defined in FuelUsed DocType
-        fuel_entry = frappe.get_doc({
+        fuel_entry_data = {
             "doctype": "Fuel Entry",
             "date": self.date,
             "fuel_tanker": self.fuel_tanker,
@@ -39,8 +55,27 @@ class FuelUsed(Document):
             "litres_dispensed": self.fuel_issued_lts,  # Fuel issued (used) from this document
             "current_balance": previous_balance - self.fuel_issued_lts,  # Subtract used fuel from previous balance
             "fuel_utilization_id": self.name,  # Link back to this Fuel Used document
-        })
+        }
+
+        # Calculate the difference based on resource type
+        if self.resource_type == "Truck":
+            fuel_entry_data["diff_odometer"] = self.odometer_km - self.previous_odometer_km
+        elif self.resource_type == "Equipment":
+            fuel_entry_data["diff_hours_copy"] = self.hours_copy - self.previous_hours_copy
+
+        fuel_entry = frappe.get_doc(fuel_entry_data)
         fuel_entry.flags.ignore_permissions = True  # If necessary to bypass permission checks
         fuel_entry.insert()
         fuel_entry.submit()
+
+    def update_resource_usage(self):
+        resource = frappe.get_doc("Resource", self.resource)
+        
+        if self.resource_type == "Truck":
+            resource.current_odometer = self.odometer_km
+        elif self.resource_type == "Equipment":
+            resource.current_hours_copy = self.hours_copy
+        
+        resource.save()
+
 
